5808bab4bc9391e793f9e162ad5f7cb831907465
HugoMorales <hugo.camargo87@gmail.com>
2025-06-24 23:42:27 -0300

    Walls

--- scripts/main_character/main_character.gd
+++ scripts/main_character/main_character.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 
-#base speed of the character
-const base_speed = 250
+const base_speed = 250	##base speed of the character
 
 func get_input():
 	var input_direction = Input.get_vector("mv_left", "mv_right", "mv_up", "mv_down")
@@ -9,14 +8,12 @@ func get_input():
 	var input_x_stick = Input.get_axis("ax_left", "ax_right")
 	
 	#simple if to make compatible with both dpad and stick
-	if $VisibleOnScreenNotifier2D.is_on_screen():
-		if input_direction != Vector2(0,0):
-			velocity = input_direction * base_speed
-		else:
-			velocity = Vector2(input_x_stick * base_speed, input_y_stick * base_speed)
+	if input_direction != Vector2(0,0):
+		velocity = input_direction * base_speed
 	else:
-		position = Vector2(250, 240)
+		velocity = Vector2(input_x_stick * base_speed, input_y_stick * base_speed)
 	
-func _physics_process(delta):
+
+func _physics_process(_delta):
 	get_input()
-	move_and_slide()
+	print(move_and_slide())
